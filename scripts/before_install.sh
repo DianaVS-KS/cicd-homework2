a5c4d2fd25c2d4b4da7aa10369b8a7f2c91acd39
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-13 20:04:52 -0500

    .

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,15 +5,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
-
-sudo dpkg --purge codedeploy-agent
-sleep 15
-cd /opt
-sudo rm -r codedeploy-agent/
-sleep 5
-cd /home/ubuntu/
-sudo .install auto > /tmp/logfile
-sleep 5
-
 sudo rm -r /home/ubuntu/cicd2
 mkdir /home/ubuntu/cicd2
