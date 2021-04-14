de7f3d490478532d2091913299bd0e427567b765
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-13 19:22:04 -0500

    before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,13 +5,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
-#create our working directory if it doesnt exist
-DIR="/home/ubuntu/cicd2"
 sudo rm -r /home/ubuntu/cicd2
-
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+mkdir /home/ubuntu/cicd2
