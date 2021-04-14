eeac2e370d18fd66d0098a6618092192a237485b
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-13 19:54:06 -0500

    x.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,10 +6,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 sudo dpkg --purge codedeploy-agent
+sleep 10
 sudo rm -r /opt/codedeploy-agent/
+sleep 3
 sudo /home/ubuntu/install auto > /tmp/logfile
 
-sleep 10
+sleep 5
 
 sudo rm -r /home/ubuntu/cicd2
 mkdir /home/ubuntu/cicd2
